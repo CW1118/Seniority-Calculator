0034ae3ccd3c15d5356319bbede50b0b08bf3e5d
Coty Williams <1118cw@gmail.com>
2015-04-03 21:43:31 -0400

    add employee picks to list

--- senior.py
+++ senior.py
@@ -1,6 +1,17 @@
 employee_count = int(raw_input("How many employees are bidding?\n>"))
 names = []
+schedules = []
+picks = []
+final = []
+
+#adding names and schedule numbers to list
 
 for i in range(1, employee_count + 1):
-	name = raw_input("Enter employee number %d's name:" % i)
-	names.append(name)
+	names.append(raw_input("Enter employee number %d's name:" % i))
+	schedules.append(i)
+
+#adding employee picks to list
+	
+for i in range(0, employee_count):
+	picks.append(map(str, raw_input("Enter %s's selections in order without any spaces:\n>" % names[i]))) #converts selections to list with map function
+	
