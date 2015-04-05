12d496981c499ad3918539266e3ab7b5c43fc5ce
Coty Williams <1118cw@gmail.com>
2015-04-05 14:17:11 -0400

    add start() function

--- senior.py
+++ senior.py
@@ -4,12 +4,37 @@ schedules = []	#list of each available schedule
 picks = []	#nested list of each employees choices
 final = []	#list of each employee and their awarded schedule
 
+def start():
+	print "What would you like to do?"
+	print "1) Start a new session"
+	print "2) View current session"
+	print "3) Add employee(s) to current session"
+	print "4) Remove employee(s) from current session"
+	
+	next = raw_input("> ")
+	
+	while True:
+		if next == "1":
+			add(True)	#for 'add' function make it to where True runs a new session and False will add to existing session
+		
+		elif next == "2":
+			view()		#pulls up the last saved txt file with all the assigned schedules
+		
+		elif next == "3":
+			add(False)
+		
+		elif next == "4":
+			remove()	#removes employee from txt doc and all lists
+		
+		else:
+			print "I don't understand. Try typing 1, 2, 3, or 4."
+			
 #finds a choice within the nested lists of picks
 def find(employee, choice_num):
 	nested_list = picks[employee]
 	choice = nested_list[choice_num]
 	return choice
-
+	
 #adding names and schedule numbers to list
 for i in range(1, employee_count + 1):
 	names.append(raw_input("Enter employee number %d's name: " % i))
