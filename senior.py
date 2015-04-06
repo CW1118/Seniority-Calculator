feb5c9bf56cecc11ac7fc05d1a4251645d3d9f61
Coty Williams <1118cw@gmail.com>
2015-04-06 17:26:48 -0400

    add session function

--- senior.py
+++ senior.py
@@ -1,27 +1,21 @@
-employee_count = int(raw_input("How many employees are bidding?\n> "))
-names = []	#list of each employees name
-schedules = []	#list of each available schedule
-picks = []	#nested list of each employees choices
-final = []	#list of each employee and their awarded schedule
-
 def start():
 	print "What would you like to do?"
 	print "1) Start a new session"
-	print "2) View current session"
-	print "3) Add employee(s) to current session"
+	print "2) View most recent session"
+	print "3) Add new employee(s) to current session"
 	print "4) Remove employee(s) from current session"
 	
-	next = raw_input("> ")
-	
 	while True:
+		next = raw_input("> ")
+		
 		if next == "1":
-			add(True)	#for 'add' function make it to where True runs a new session and False will add to existing session
+			session(True)
 		
 		elif next == "2":
-			view()		#pulls up the last saved txt file with all the assigned schedules
+			session(False)		#pulls up the last saved txt file with all the assigned schedules
 		
 		elif next == "3":
-			add(False)
+			add()
 		
 		elif next == "4":
 			remove()	#removes employee from txt doc and all lists
@@ -29,22 +23,54 @@ def start():
 		else:
 			print "I don't understand. Try typing 1, 2, 3, or 4."
 			
+def session(new):
+	if new == True:
+		employee_count = int(raw_input("How many employees are bidding?\n> "))
+		names = []	#list of each employees name
+		schedules = []	#list of each available schedule
+		picks = []	#nested list of each employees choices
+		final = []	#list of each employee and their awarded schedule
+		
+		#adding names and schedule numbers to an empty list
+		for i in range(1, employee_count + 1):
+			names.append(raw_input("Enter employee number %d's name: " % i))
+			schedules.append(str(i))
+			
+		#adding employee picks to list	
+		for i in range(0, employee_count):
+			#converts selections to list with map function
+			picks.append(map(str, raw_input("Enter %s's selections in order without any spaces:\n> " % names[i])))
+			
+		print "Would you like to save and assign schedules?"
+		print "1) Yes"
+		print "2) No"
+		
+		while True:
+			next = raw_input("> ")
+			
+			if next == "1":
+				assign()
+			
+			elif next == "2":
+				start()
+				
+			else:
+				print "I don't understand. Try typing 1 or 2."
+				
+	else:
+		with open('Schedule.txt', 'a+') as file:
+			print file.read()
+			file.close()
+			start()
+		
 #finds a choice within the nested lists of picks
 def find(employee, choice_num):
 	nested_list = picks[employee]
 	choice = nested_list[choice_num]
 	return choice
 	
-#adding names and schedule numbers to list
-for i in range(1, employee_count + 1):
-	names.append(raw_input("Enter employee number %d's name: " % i))
-	schedules.append(str(i))
-
-#adding employee picks to list	
-for i in range(0, employee_count):
-	#converts selections to list with map function
-	picks.append(map(str, raw_input("Enter %s's selections in order without any spaces:\n> " % names[i])))
-	
+start()
+		
 #goes through employees 1 by 1 and assigns schedules
 for employee in range(0, employee_count):
 	choice_num = 0		
