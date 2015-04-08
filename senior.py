f3d9cb86ffbe697129ea715a6e2996d84f3faec3
Coty Williams <1118cw@gmail.com>
2015-04-07 22:51:33 -0400

    add assign() function and change employee_count into global variable

--- senior.py
+++ senior.py
@@ -1,5 +1,10 @@
 from sys import exit
 
+names = []	#list of each employees name
+schedules = []	#list of each available schedule
+picks = []	#nested list of each employees choices
+final = []	#list of each employee and their awarded schedule
+
 def start():
 	print "What would you like to do?"
 	print "1) Start a new session"
@@ -20,16 +25,13 @@ def start():
 		
 		else:
 			print "I don't understand. Try typing 1, 2, or 3."
-
+	
 #if argument is True then session() starts taking input for a new schedule 
 #else if argument is False then session() will display the previous schedule	
 def session(new):
 	if new == True:
+		global employee_count
 		employee_count = int(raw_input("How many employees are bidding?\n> "))
-		names = []	#list of each employees name
-		schedules = []	#list of each available schedule
-		picks = []	#nested list of each employees choices
-		final = []	#list of each employee and their awarded schedule
 		
 		#adding names and schedule numbers to an empty list
 		for i in range(1, employee_count + 1):
@@ -61,36 +63,36 @@ def session(new):
 	else:
 		with open('Schedule.txt', 'a+') as file:
 			print file.read()
-			file.close()
 			start()
-		
+
 #finds a choice within the nested lists of picks
 def find(employee, choice_num):
 	nested_list = picks[employee]
 	choice = nested_list[choice_num]
 	return choice
-	
-start()
 		
 #goes through employees 1 by 1 and assigns schedules
-for employee in range(0, employee_count):
-	choice_num = 0		
+def assign():	
+	for employee in range(0, employee_count):
+		choice_num = 0		
 	
-	while len(final) < employee_count:	
-		choice = find(employee, choice_num)
+		while len(final) < employee_count:	
+			choice = find(employee, choice_num)
 
-		if choice in schedules:
-			#if the employees choice is available this will assign it to them		
-			final.append("%s has been awarded schedule number: %s" % (names[employee], choice))
-			#deletes choice from schedules list so it cant be assigned again
-			schedules.remove(choice)
-			#breaks out of the while loop to move on to the next employee
-			break	
+			if choice in schedules:
+				#if the employees choice is available this will assign it to them		
+				final.append("%s has been awarded schedule number: %s" % (names[employee], choice))
+				#deletes choice from schedules list so it cant be assigned again
+				schedules.remove(choice)
+				#breaks out of the while loop to move on to the next employee
+				break	
 		
-		else:
-			#if the employees 1st choice is taken this will add one to 
-			#the choice_num variable which will check the availability of their next choice
-			choice_num += 1	
-			continue
+			else:
+				#if the employees 1st choice is taken this will add one to 
+				#the choice_num variable which will check the availability of their next choice
+				choice_num += 1	
+				continue
+				
+	print '\n'.join(final)
 			
-print '\n'.join(final)
+start()
